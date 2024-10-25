418a6d420f0d432263e0a26db37a3f541c45bd79
Phil._.T <142722387+PhilTilleman@users.noreply.github.com>
2024-10-25 15:05:47 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('.contact form');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        alert("Message sent successfully!");
+        form.reset();
+    });
+});
